18fd69dacbe6258742761b0193def9ebdff83d92
Daniel Marcus <therealdanmarcus@gmail.com>
2017-03-12 13:48:15 +0000

    Add support for MDL to Main

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
 
 class Main extends Component {
+   componentDidMount() {
+    // This upgrades all upgradable components (i.e. with 'mdl-js-*' class)
+    if (typeof componentHandler === 'object') { 
+      componentHandler.upgradeDom();
+    }
+
+   }
+   componentDidUpdate() {
+    // This upgrades all upgradable components (i.e. with 'mdl-js-*' class)
+    if (typeof componentHandler === 'object') { 
+      componentHandler.upgradeDom();
+    }
+    
+  }
+  
   render() {
-    return (<p>This was rendered by React because we are at location '/'.  </p>);
+    return (<div>
+      <p>This was rendered by React because we are at location '/'.</p>
+      <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        Button
+      </button>
+    </div>);
   }
 }
 
